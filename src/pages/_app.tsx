bf24b0e91e0b477cd43273bd1c7512b827b45844
Aldovani <aldovane.etec@gmail.com>
2021-05-23 17:31:49 -0300

    correçao bug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { Header } from "../components/Header";
+import { Header } from "../components/header";
 import { ScoreContextProvider } from "../contexts/ScorerContext";
 import "../styles/globals.css";
 
